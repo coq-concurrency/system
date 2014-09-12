fb33cb18d129d39bae1aad888a249a6bc8c3d59c
Guillaume Claret <dev@clarus.me>
2014-09-12 16:56:07 +0200

    Socket.v added

---
+++ Socket.v
@@ -0,0 +1,9 @@
+(** Experiments for a TCP sockets API. *)
+Require Import Coq.Lists.List.
+Require Import Coq.Strings.String.
+Require Import Computation.
+
+Import ListNotations.
+Import C.Notations.
+Open Local Scope string.
+
