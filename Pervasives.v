a013c018767f3397ba328292945af38b38e2390b
Guillaume Claret <dev@clarus.me>
2014-09-25 11:36:32 +0200

    New protocol with client and server sockets separated

--- Pervasives.v
+++ Pervasives.v
@@ -9,8 +9,8 @@ Open Local Scope string.
 (** Events to log data. *)
 Module Log.
   Module Output.
-    Inductive t : Type :=
-    | write : string -> t.
+    Inductive t : Set :=
+    | write (message : string).
   End Output.
 End Log.
 
@@ -19,7 +19,7 @@ Module File.
   (** The name of a file. Only relative for now. *)
   Module Name.
     (** A file name is a path and a base name. *)
-    Record t : Type := {
+    Record t : Set := {
       path : list string;
       base : string }.
 
@@ -53,27 +53,23 @@ Module File.
 
   (** Incoming events. *)
   Module Input.
-    Inductive t : Type :=
-    | read : Name.t -> string -> t (** The file content had been read. *).
+    Inductive t : Set :=
+    | read ( file_name : Name.t) (content : string)
+      (** All the file content had been read. *).
   End Input.
 
   (** Generated events. *)
   Module Output.
-    Inductive t : Type :=
-    | read : Name.t -> t (** Read all the content of a file. *).
+    Inductive t : Set :=
+    | read (file_name : Name.t) (** Read all the content of a file. *).
   End Output.
 End File.
 
-Module TCPServerSocket.
+Module TCPClientSocket.
   Module Id.
-    Inductive t : Type :=
-    | new : nat -> t.
-  End Id.
-
-  Module ConnectionId.
     Require Import Coq.Numbers.Natural.Peano.NPeano.
 
-    Inductive t : Type :=
+    Inductive t : Set :=
     | new : nat -> t.
 
     (** Test equality. *)
@@ -81,32 +77,50 @@ Module TCPServerSocket.
       match (id1, id2) with
       | (new id1, new id2) => Nat.eqb id1 id2
       end.
-  End ConnectionId.
+  End Id.
 
   Module Input.
-    Inductive t : Type :=
-    | bound : Id.t -> t
-    | accepted : ConnectionId.t -> t
-    | read : ConnectionId.t -> string -> t.
+    Inductive t : Set :=
+    | accepted (id : Id.t)
+    | read (id : Id.t) (content : string).
   End Input.
 
   Module Output.
-    Inductive t : Type :=
-    | bind : nat -> t
-    | write : ConnectionId.t -> string -> t
-    | close_connection : ConnectionId.t -> t.
+    Inductive t : Set :=
+    | write (id : Id.t) (message : string)
+    | close (id : Id.t).
+  End Output.
+End TCPClientSocket.
+
+Module TCPServerSocket.
+  Module Id.
+    Inductive t : Set :=
+    | new : nat -> t.
+  End Id.
+
+  Module Input.
+    Inductive t : Set :=
+    | bound (id : Id.t).
+  End Input.
+
+  Module Output.
+    Inductive t : Set :=
+    | bind (port : nat)
+    | close (id : Id.t).
   End Output.
 End TCPServerSocket.
 
 Module Input.
-  Inductive t : Type :=
+  Inductive t : Set :=
   | file : File.Input.t -> t
-  | socket : TCPServerSocket.Input.t -> t.
+  | client_socket : TCPClientSocket.Input.t -> t
+  | server_socket : TCPServerSocket.Input.t -> t.
 End Input.
 
 Module Output.
-  Inductive t : Type :=
+  Inductive t : Set :=
   | log : Log.Output.t -> t
   | file : File.Output.t -> t
-  | socket : TCPServerSocket.Output.t -> t.
+  | client_socket : TCPClientSocket.Output.t -> t
+  | server_socket : TCPServerSocket.Output.t -> t.
 End Output.
--- StdLib.v
+++ StdLib.v
@@ -24,17 +24,27 @@ Module File.
     C.write (Output.file (File.Output.read file_name)).
 End File.
 
+(** TCP client socket. *)
+Module TCPClientSocket.
+  (** Write a message. *)
+  Definition write {sig : Signature.t} (id : TCPClientSocket.Id.t)
+    (data : string) : C sig unit :=
+    C.write (Output.client_socket (TCPClientSocket.Output.write id data)).
+
+  (** Close a socket. *)
+  Definition close {sig : Signature.t} (id : TCPClientSocket.Id.t)
+    : C sig unit :=
+    C.write (Output.client_socket (TCPClientSocket.Output.close id)).
+End TCPClientSocket.
+
 (** TCP server socket. *)
 Module TCPServerSocket.
   (** Bind a socket. *)
   Definition bind {sig : Signature.t} (port : nat) : C sig unit :=
-    C.write (Output.socket (TCPServerSocket.Output.bind port)).
+    C.write (Output.server_socket (TCPServerSocket.Output.bind port)).
 
-  Definition write {sig : Signature.t} (id : TCPServerSocket.ConnectionId.t)
-    (data : string) : C sig unit :=
-    C.write (Output.socket (TCPServerSocket.Output.write id data)).
-
-  Definition close_connection {sig : Signature.t}
-    (id : TCPServerSocket.ConnectionId.t) : C sig unit :=
-    C.write (Output.socket (TCPServerSocket.Output.close_connection id)).
+  (** Close a socket. *)
+  Definition close {sig : Signature.t} (id : TCPServerSocket.Id.t)
+    : C sig unit :=
+    C.write (Output.server_socket (TCPServerSocket.Output.close id)).
 End TCPServerSocket.
