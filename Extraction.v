e8f2d598216ed37fc899c827c2c03ecbec89f749
Guillaume Claret <dev@clarus.me>
2014-09-26 08:22:18 +0200

    Native module to separate constants with specific extraction

--- Extraction.v
+++ Extraction.v
@@ -8,23 +8,32 @@ Require Import StdLib.
 Import ListNotations.
 Open Local Scope string.
 
-Module MLString.
-  Parameter t : Set.
-  Extract Constant t => "string".
+Module Native.
+  Module String.
+    Parameter t : Set.
+    Extract Constant t => "string".
 
-  Parameter append : t -> t -> t.
+    Parameter append : t -> t -> t.
 
-  Parameter to_string : t -> string.
-  Parameter of_string : string -> t.
+    Parameter to_string : t -> string.
+    Parameter of_string : string -> t.
 
-  Parameter to_nat : t -> option nat.
-  Parameter of_nat : nat -> t.
-End MLString.
+    Parameter to_nat : t -> option nat.
+    Parameter of_nat : nat -> t.
+  End String.
 
-Module Base64.
-  Parameter encode : MLString.t -> MLString.t.
-  Parameter decode : MLString.t -> MLString.t.
-End Base64.
+  Module Base64.
+    Parameter encode : String.t -> String.t.
+    Parameter decode : String.t -> String.t.
+  End Base64.
+
+  Module Process.
+    Parameter t : Set.
+    Parameter run : String.t -> t.
+    Parameter print_line : String.t -> t -> unit.
+    Parameter read_line : t -> String.t.
+  End Process.
+End Native.
 
 (** Import input events. *)
 Module Input.
@@ -34,44 +43,44 @@ Module Input.
     | tcp_client_socket_accepted | tcp_client_socket_read
     | tcp_server_socket_bound.
 
-    Parameter of_ml_string : MLString.t -> option t.
+    Parameter of_native_string : Native.String.t -> option t.
   End Command.
 
-  Definition import_file_read (file_name : MLString.t) (content : MLString.t)
+  Definition import_file_read (file_name : Native.String.t) (content : Native.String.t)
     : Input.t :=
-    let file_name := MLString.to_string (Base64.decode file_name) in
-    let content := MLString.to_string (Base64.decode content) in
+    let file_name := Native.String.to_string (Native.Base64.decode file_name) in
+    let content := Native.String.to_string (Native.Base64.decode content) in
     Input.file (File.Input.read file_name content).
 
-  Parameter tokenize : MLString.t -> list MLString.t.
+  Parameter tokenize : Native.String.t -> list Native.String.t.
 
-  Definition import (input : MLString.t) : Input.t + string :=
+  Definition import (input : Native.String.t) : Input.t + string :=
     match tokenize input with
     | [] => inr "The input cannot be empty."
     | command :: arguments =>
-      match (Command.of_ml_string input, arguments) with
+      match (Command.of_native_string input, arguments) with
       | (None, _) => inr "Invalid command."
       | (Some Command.file_read, [file_name; content]) =>
-        let file_name := MLString.to_string (Base64.decode file_name) in
-        let content := MLString.to_string (Base64.decode content) in
+        let file_name := Native.String.to_string (Native.Base64.decode file_name) in
+        let content := Native.String.to_string (Native.Base64.decode content) in
         inl (Input.file (File.Input.read file_name content))
       | (Some Command.tcp_client_socket_accepted, [id]) =>
-        match MLString.to_nat id with
+        match Native.String.to_nat id with
         | None => inr "Expected an integer."
         | Some id =>
           let id := TCPClientSocket.Id.new id in
           inl (Input.client_socket (TCPClientSocket.Input.accepted id))
         end
       | (Some Command.tcp_client_socket_read, [id; content]) =>
-        match MLString.to_nat id with
+        match Native.String.to_nat id with
         | None => inr "Expected an integer."
         | Some id =>
           let id := TCPClientSocket.Id.new id in
-          let content := MLString.to_string (Base64.decode content) in
+          let content := Native.String.to_string (Native.Base64.decode content) in
           inl (Input.client_socket (TCPClientSocket.Input.read id content))
         end
       | (Some Command.tcp_server_socket_bound, [id]) =>
-        match MLString.to_nat id with
+        match Native.String.to_nat id with
         | None => inr "Expected an integer."
         | Some id =>
           let id := TCPServerSocket.Id.new id in
@@ -84,16 +93,16 @@ End Input.
 
 (** Export output events. *)
 Module Output.
-  Definition join (s1 s2 : MLString.t) : MLString.t :=
-    MLString.append (MLString.append s1 (MLString.of_string " ")) s2.
+  Definition join (s1 s2 : Native.String.t) : Native.String.t :=
+    Native.String.append (Native.String.append s1 (Native.String.of_string " ")) s2.
 
-  Definition export (output : Output.t) : MLString.t :=
-    let string s := MLString.of_string s in
-    let base64 s := Base64.encode (MLString.of_string s) in
+  Definition export (output : Output.t) : Native.String.t :=
+    let string s := Native.String.of_string s in
+    let base64 s := Native.Base64.encode (Native.String.of_string s) in
     let client_id id :=
-      MLString.of_nat (match id with TCPClientSocket.Id.new id => id end) in
+      Native.String.of_nat (match id with TCPClientSocket.Id.new id => id end) in
     let server_id id :=
-      MLString.of_nat (match id with TCPServerSocket.Id.new id => id end) in
+      Native.String.of_nat (match id with TCPServerSocket.Id.new id => id end) in
     match output with
     | Output.log (Log.Output.write message) =>
       join (string "Log.write") (base64 message)
@@ -105,12 +114,20 @@ Module Output.
     | Output.client_socket (TCPClientSocket.Output.close id) =>
       join (string "TCPClientSocket.close") (client_id id)
     | Output.server_socket (TCPServerSocket.Output.bind port) =>
-      let port := MLString.of_nat port in
+      let port := Native.String.of_nat port in
       join (string "TCPServerSocket.bind") port
     | Output.server_socket (TCPServerSocket.Output.close id) =>
       join (string "TCPServerSocket.close") (server_id id)
     end.
 End Output.
+
+(*Definition run_ocaml (sig : Signature.t) (mem : Memory.t sig)
+  (start : unit -> C sig unit) (handler : Input.t -> C sig unit) : unit :=
+  let last triple :=
+    match triple with
+    | (x, y, z) => (y, z)
+    end in
+  let (mem, outputs) := last (run (start tt)) in*)
 
 (*
 (** * A nice extraction for strings. *)
