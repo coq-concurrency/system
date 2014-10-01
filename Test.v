8b274a09fb9f6e775d1867a0d47b78af12d0693c
Guillaume Claret <dev@clarus.me>
2014-10-01 10:01:54 +0200

    ReadFile example working

--- Test.v
+++ Test.v
@@ -51,23 +51,22 @@ End HelloWorld.
 (** Print the content of a file. *)
 Module ReadFile.
   (** The file to open. *)
-  Definition resolv : string := "README.md".
+  Definition file_name : string := "README.md".
 
   (** Start the program. *)
   Definition start {sig : Signature.t} (_ : unit) : C sig unit :=
-    File.read resolv.
+    C.Send (Output.New Command.FileRead 0 file_name).
 
   (** Handle events. *)
   Definition handle {sig : Signature.t} (input : Input.t) : C sig unit :=
     match input with
-    | Input.File input =>
-      match input with
-      | File.Input.Read _ data => Log.write data
-      end
+    | Input.New Command.FileRead _ (Some content) =>
+      C.Send (Output.New Command.Log 0 content)
+    | Input.New Command.Log _ _ => C.Exit tt
     | _ => C.Ret tt
     end.
 
-  (** Run the program sequentially on a list of input events. *)
+  (*(** Run the program sequentially on a list of input events. *)
   Definition run (inputs : list File.Input.t) : list Output.t :=
     let inputs := List.map Input.File inputs in
     let program :=
@@ -78,10 +77,10 @@ Module ReadFile.
     end.
 
 
-  Check eq_refl : run [] = [Output.File (File.Output.Read resolv)].
-  Check eq_refl : run [File.Input.Read resolv "nameserver 34.123.45.46"] = [
+  Check eq_refl : run [] = [Output.File (File.Output.Read file_name)].
+  Check eq_refl : run [File.Input.Read file_name "nameserver 34.123.45.46"] = [
     Output.Log (Log.Output.Write "nameserver 34.123.45.46");
-    Output.File (File.Output.Read resolv)].
+    Output.File (File.Output.Read file_name)].*)
 
   Definition read_file := Extraction.run _ Memory.Nil start handle.
   Extraction "tests/readFile" read_file.
