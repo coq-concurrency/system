a79a62a371db1761deb2cb1e0f098070a8f66baa
Guillaume Claret <dev@clarus.me>
2015-08-11 13:38:43 +0200

    Typo

--- pp.rb
+++ pp.rb
@@ -4,7 +4,7 @@ require 'erb'
 # The version of Coq.
 version = `coqc -v`.match(/version ([^(]*) \(/)[1]
 
-Dir.glob("*.v.erb") do |file_name|
+Dir.glob("src/*.v.erb") do |file_name|
   renderer = ERB.new(File.read(file_name, encoding: "UTF-8"))
   output_name = file_name[0..-5]
   File.open(output_name, "w") do |file|
