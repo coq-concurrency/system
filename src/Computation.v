f0a71f4706c8a5012e3d523dbee3e645ed303cc0
Guillaume Claret <dev@clarus.me>
2015-08-11 13:54:33 +0200

    Computation.v.erb removed

--- src/Computation.v
+++ src/Computation.v
@@ -14,18 +14,18 @@ Module Memory.
   (** The shared memory is a list of typed cells. *)
   Inductive t : Signature.t -> Type :=
   | Nil : t []
-  | Cons : forall {A : Type} {sig : Signature.t}, A -> t sig -> t (A :: sig).
-
+  | Cons : forall (A : Type) (sig : Signature.t), A -> t sig -> t (A :: sig).
+  
   (** The first shared memory cell. *)
   Definition head {A : Type} {sig : Signature.t} (mem : t (A :: sig)) : A :=
     match mem with
-    | Cons <%= "_ _" if version[0..2] == "8.4" %> x _ => x
+    | Cons _ _ x _ => x
     end.
-
+  
   (** The tail of the shared memory. *)
   Definition tail {A : Type} {sig : Signature.t} (mem : t (A :: sig)) : t sig :=
     match mem with
-    | Cons <%= "_ _" if version[0..2] == "8.4" %> _ mem => mem
+    | Cons _ _ _ mem => mem
     end.
 End Memory.
 
@@ -37,21 +37,22 @@ Module Ref.
 
   Instance cons_left (A : Type) (sig : Signature.t) : C A (A :: sig) := {
     read mem := Memory.head mem;
-    write mem x := Memory.Cons x (Memory.tail mem) }.
+    write mem x := Memory.Cons _ _ x (Memory.tail mem) }.
 
   Instance cons_right (A B : Type) (sig : Signature.t) (I : C A sig)
     : C A (B :: sig) := {
     read mem := read (Memory.tail mem);
-    write mem x := Memory.Cons (Memory.head mem) (write (Memory.tail mem) x) }.
+    write mem x :=
+      Memory.Cons _ _ (Memory.head mem) (write (Memory.tail mem) x) }.
 End Ref.
 
 (** Definition of a computation. *)
 Module C.
   (** A computation is a free monad with some primitives. *)
   Inductive t (sig : Signature.t) : Type -> Type :=
-  | Ret : forall {A : Type}, A -> t sig A
-  | Bind : forall {A B : Type}, t sig A -> (A -> t sig B) -> t sig B
-  | Read : forall {A : Type}, `{Ref.C A sig} -> t sig A
+  | Ret : forall (A : Type), A -> t sig A
+  | Bind : forall (A B : Type), t sig A -> (A -> t sig B) -> t sig B
+  | Read : forall (A : Type), `{Ref.C A sig} -> t sig A
     (** Read the value of a reference (atomic). *)
   | Write : forall {A : Type}, `{Ref.C A sig} -> A -> t sig unit
     (** Write a value in a reference (atomic). *)
@@ -81,20 +82,3 @@ Module C.
       (at level 200, B at level 200).
   End Notations.
 End C.
-
-(*
-(** Functions on lists. *)
-Module List.
-  Import C.Notations.
-
-  (** Iterate a computation over a list. *)
-  Fixpoint iter (sig : Signature.t) (A : Type) (l : list A)
-    (f : A -> C.t sig unit) : C.t sig unit :=
-    match l with
-    | [] => C.Ret tt
-    | x :: l =>
-      do! f x in
-      iter _ _ l f
-    end.
-  Arguments iter [sig A] _ _.
-End List.*)
